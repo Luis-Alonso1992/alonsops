d5de5db73675508e5c80a4e4edd6ba92fa3b9979
Luis Alonso <lalonso@forthepeople.com>
2025-11-03 14:12:14 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.19.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+
+resource "aws_lightsail_instance" "alonsops-vps" {
+  name              = "alonsops-vps"
+  availability_zone = "us-east-1b"
+  blueprint_id      = "amazon_linux_2"
+  bundle_id         = "nano_3_0"
+  tags = {
+    environment = "production"
+  }
+}
