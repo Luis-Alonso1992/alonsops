7b37e61d8fc08c1652e3e3d0c76b2eb52af12a10
Luis Alonso <lalonso@forthepeople.com>
2025-11-04 12:34:12 -0500

    Added key pair name to terraform main.tf

--- main.tf
+++ main.tf
@@ -19,6 +19,7 @@ resource "aws_lightsail_instance" "alonsops-vps" {
   availability_zone = "us-east-1b"
   blueprint_id      = "amazon_linux_2"
   bundle_id         = "nano_3_0"
+  key_pair_name     = "MyKey"
   tags = {
     environment = "production"
   }
